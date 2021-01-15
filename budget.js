821975d5473439f017f43c889f370fc800251a27
Anthony De la Torre <75534476+Youspeakcubic@users.noreply.github.com>
2021-01-15 04:51:25 -0600

    Add files via upload
    expenses are now added to a table through javascript, Next update will subtract each expense from the budget you have.

--- budget.js
+++ budget.js
@@ -1,14 +1,37 @@
 let budgetCount = document.getElementById('budgetCount');
 let expenseList = document.getElementById('expenseItems');
+let budgetTable = document.getElementById('tableBudget');
+let income;
 function addBudget() {
-  let income = document.getElementById("incomeInput").value;
-  budgetCount.appendChild(document.createTextNode(` $${income}`));
+  income = document.getElementById("incomeInput").value;
+  if (!income) {
+
+  } else {
+    budgetCount.innerHTML = "";
+    budgetCount.appendChild(document.createTextNode(`Budget: $${income}`));
+
+  }
 }
 function addExpense() {
-  let expenseName = document.getElementById('expenseName').value;
-  let expenseCost = document.getElementById('expenseCost').value;
-  let expenseType = document.getElementById('expenseType').value;
-  let liNode = document.createElement("LI");
-  liNode.appendChild(document.createTextNode(`Service: ${expenseName} Cost:$${expenseCost} Type: ${expenseType}`))
-  expenseList.appendChild(liNode);
+  let expense =[
+    document.getElementById('expenseName').value ,
+    document.getElementById('expenseCost').value,
+    document.getElementById('expenseType').value
+ ];
+if (!expense[0] || !expense[1] ) {
+    expenseCost.placeholder = "Please enter a value";
+    expenseName.placeholder = "please enter a value";
+  } else {
+    let row = expenseList.insertRow(-1);
+    let cell1 = row.insertCell(0);
+    let cell2 = row.insertCell(1);
+    let cell3 = row.insertCell(2);
+    cell1.innerHTML = `${expense[0]}`;
+    cell2.innerHTML = `$${expense[1]}`;
+    cell3.innerHTML = `${expense[2]}`;
+    expenseName.value = "";
+    expenseCost.value = "";
+    return `${income}` - `${expenseCost}`
+    console.log(`${income}`);
+  }
 }
