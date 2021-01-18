3b2baeab762c622cd9801929249f34cd6575aeef
Anthony De la Torre <75534476+Youspeakcubic@users.noreply.github.com>
2021-01-18 03:11:12 -0600

    Add files via upload
    Finished functionality of adding expenses to table, also added number of expenses and net gain in footer. Still need mobile responsiveness.

--- budget.js
+++ budget.js
@@ -1,24 +1,30 @@
-let budgetCount = document.getElementById('budgetCount');
-let expenseList = document.getElementById('expenseItems');
-let budgetTable = document.getElementById('tableBudget');
+let budgetCount = document.getElementById("budgetCount");
+let expenseList = document.getElementById("expenseItems");
+let budgetTable = document.getElementById("tableBudget");
+let expenseCounter = document.getElementById("expenseCounter");
+let netFlow = document.getElementById("netFlow");
 let income;
+let expCounter = 0;
 function addBudget() {
   income = document.getElementById("incomeInput").value;
   if (!income) {
-
   } else {
     budgetCount.innerHTML = "";
+    budgetTable.innerHTML ="";
     budgetCount.appendChild(document.createTextNode(`Budget: $${income}`));
-
+    budgetTable.appendChild(document.createTextNode(`Budget: $${income}`));
+    document.getElementById("incomeInput").value = "";
   }
 }
 function addExpense() {
-  let expense =[
-    document.getElementById('expenseName').value ,
-    document.getElementById('expenseCost').value,
-    document.getElementById('expenseType').value
- ];
-if (!expense[0] || !expense[1] ) {
+  expenseCounter.innerHTML = "";
+  let expense = [
+    document.getElementById("expenseName").value,
+    document.getElementById("expenseType").value,
+    document.getElementById("expenseCost").value
+  ];
+  income = income - expense[2];
+  if (!expense[0] || !expense[1]) {
     expenseCost.placeholder = "Please enter a value";
     expenseName.placeholder = "please enter a value";
   } else {
@@ -26,12 +32,15 @@ if (!expense[0] || !expense[1] ) {
     let cell1 = row.insertCell(0);
     let cell2 = row.insertCell(1);
     let cell3 = row.insertCell(2);
+    let cell4 = row.insertCell(3);
     cell1.innerHTML = `${expense[0]}`;
-    cell2.innerHTML = `$${expense[1]}`;
-    cell3.innerHTML = `${expense[2]}`;
+    cell2.innerHTML = `${expense[1]}`;
+    cell3.innerHTML = `$${expense[2]}`;
+    cell4.innerHTML = `Budget: $${income}`;
     expenseName.value = "";
     expenseCost.value = "";
-    return `${income}` - `${expenseCost}`
-    console.log(`${income}`);
+    expCounter++;
+    expenseCounter.appendChild(document.createTextNode(`#${expCounter} expenses`));
+    netFlow.appendChild(document.createTextNode(`$${income}`));
   }
 }
